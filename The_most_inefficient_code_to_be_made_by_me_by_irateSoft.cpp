07ee55decad8dfa74cdcb83629fdd4538137889a
Programlover305 <adamestrada879@gmail.com>
2025-08-02 13:35:15 -0700

    Add files via upload

---
+++ The_most_inefficient_code_to_be_made_by_me_by_irateSoft.cpp
@@ -0,0 +1,343 @@
+//# including the iostream library
+#include <iostream>
+// #include the string library
+#include <string>
+// #include the algroithm library
+#include <algorithm>
+
+// This is the display messgae function
+// In which it displays the message
+void Display_message() {
+    //The definition of word1
+    std::string word1 = "Man,\n";
+    //The definition of word2
+    std::string word2 = "this\n";
+    //The definition of word3
+    std::string word3 = "code\n";
+    //The definition of word4
+    std::string word4 = "is\n";
+    //The definition of word5
+    std::string word5 = "so\n";
+    //The definition of word6
+    std::string word6 = "in-\n";
+    //The definition of word7
+    std::string word7 = "e-\n";
+    //The definition of word8
+    std::string word8 = "ff-\n";
+    //The definition of word9
+    std::string word9 = "i-\n";
+    //The definition of word10
+    std::string word10 = "cient\n";
+    //The definition of word11
+    std::string word11 = "it's\n";
+    //The definition of word12
+    std::string word12 = "like\n";
+    //The definition of word13
+    std::string word13 = "this\n";
+    //The definition of word14
+    std::string word14 = "was\n";
+    //The definition of word15
+    std::string word15 = "made\n";
+    //The definition of word16
+    std::string word16 = "by\n";
+    //The definition of word17
+    std::string word17 = "some-\n";
+    //The definition of word18
+    std::string word18 = "one\n";
+    //The definition of word19
+    std::string word19 = "by\n";
+    //The definition of word20
+    std::string word20 = "the\n";
+    //The definition of word21
+    std::string word21 = "name\n";
+    //The definition of word22
+    std::string word22 = "of\n";
+    //The definition of word23
+    std::string word23 = "Ir-\n";
+    //The definition of word24
+    std::string word24 = "ate\n";
+    //The definition of word25
+    std::string word25 = "Soft\n";
+    //The definition of word26
+    std::string word26 = "ware\n";
+    //The definition of word27
+    std::string word27 = "who\n";
+    //The definition of word28
+    std::string word28 = "made\n";
+    //The definition of word29
+    std::string word29 = "a\n";
+    //The definition of word30
+    std::string word30 = "indie\n";
+    //The definition of word31
+    std::string word31 = "game\n";
+    //The definition of word32
+    std::string word32 = "called\n";
+    //The definition of word33
+    std::string word33 = "Heart-\n";
+    //The definition of word34
+    std::string word34 = "arrack\n";
+    //The definition of word35
+    std::string word35 = ".\n";
+    //The definition of word36
+    std::string word36 = "He\n";
+    //The definition of word37
+    std::string word37 = "sounds\n";
+    //The definition of word38
+    std::string word38 = "like\n";
+    //The definition of word39
+    std::string word39 = "a\n";
+    //The definition of word40
+    std::string word40 = "hack\n";
+    //The definition of word41
+    std::string word41 = "of\n";
+    //The definition of word42
+    std::string word42 = "a\n";
+    //The definition of word43
+    std::string word43 = "game\n";
+    //The definition of word44
+    std::string word44 = "developer\n";
+    //The definition of word45
+    std::string word45 = "and\n";
+    //The definition of word46
+    std::string word46 = "a\n";
+    //The definition of word47
+    std::string word47 = "hack\n";
+    //The definition of word48
+    std::string word48 = "Pro-\n";
+    //The definition of word49
+    std::string word49 = "grammer\n";
+    //The definition of word50
+    std::string word50 = ".\n";
+    //The definition of word51
+    std::string word51 = "Did\n";
+    //The definition of word52
+    std::string word52 = "you\n";
+    //The definition of word53
+    std::string word53 = "know\n";
+    //The definition of word54
+    std::string word54 = "that\n";
+    //The definition of word55
+    std::string word55 = "his\n";
+    //The definition of word56
+    std::string word56 = "dad\n";
+    //The definition of word57
+    std::string word57 = "works\n";
+    //The definition of word58
+    std::string word58 = "at\n";
+    //The definition of word59
+    std::string word59 = "Snow-\n";
+    //The definition of word60
+    std::string word60 = "storm\n";
+    //The definition of word61
+    std::string word61 = "?\n";
+    //The definition of word62
+    std::string word62 = "Did\n";
+    //The definition of word63
+    std::string word63 = "you\n";
+    //The definition of word64
+    std::string word64 = "know\n";
+    //The definition of word65
+    std::string word65 = "that\n";
+    //The definition of word66
+    std::string word66 = "he\n";
+    //The definition of word67
+    std::string word67 = "also\n";
+    //The definition of word68
+    std::string word68 = "has\n";
+    //The definition of word69
+    std::string word69 = "Thi\n";
+    //The definition of word70
+    std::string word70 = "-\n";
+    //The definition of word71
+    std::string word71 = "rty\n";
+    //The definition of word72
+    std::string word72 = "years\n";
+    //The definition of word73
+    std::string word73 = "of\n";
+    //The definition of word74
+    std::string word74 = "pro\n";
+    //The definition of word75
+    std::string word75 = "-\n";
+    //The definition of word76
+    std::string word76 = "gramm\n";
+    //The definition of word77
+    std::string word77 = "-\n";
+    //The definition of word78
+    std::string word78 = "ing\n";
+    //The definition of word79
+    std::string word79 = ".\n";
+    //Printing word1
+    std::cout << word1;
+    //Printing word2
+    std::cout << word2;
+    //Printing word3
+    std::cout << word3;
+    //Printing word4
+    std::cout << word4;
+    //Printing word5
+    std::cout << word5;
+    //Printing word6
+    std::cout << word6;
+    //Printing word7
+    std::cout << word7;
+    //Printing word8
+    std::cout << word8;
+    //Printing word9
+    std::cout << word9;
+    //Printing word10
+    std::cout << word10;
+    //Printing word11
+    std::cout << word11;
+    //Printing word12
+    std::cout << word12;
+    //Printing word13
+    std::cout << word13;
+    //Printing word14
+    std::cout << word14;
+    //Printing word15
+    std::cout << word15;
+    //Printing word16
+    std::cout << word16;
+    //Printing word17
+    std::cout << word17;
+    //Printing word18
+    std::cout << word18;
+    //Printing word19
+    std::cout << word19;
+    //Printing word20
+    std::cout << word20;
+    //Printing word21
+    std::cout << word21;
+    //Printing word22
+    std::cout << word22;
+    //Printing word23
+    std::cout << word23;
+    //Printing word24
+    std::cout << word24;
+    //Printing word25
+    std::cout << word25;
+    //Printing word26
+    std::cout << word26;
+    //Printing word27
+    std::cout << word27;
+    //Printing word28
+    std::cout << word28;
+    //Printing word29
+    std::cout << word29;
+    //Printing word30
+    std::cout << word30;
+    //Printing word31
+    std::cout << word31;
+    //Printing word32
+    std::cout << word32;
+    //Printing word33
+    std::cout << word33;
+    //Printing word34
+    std::cout << word34;
+    //Printing word35
+    std::cout << word35;
+    //Printing word36
+    std::cout << word36;
+    //Printing word37
+    std::cout << word37;
+    //Printing word38
+    std::cout << word38;
+    //Printing word39
+    std::cout << word39;
+    //Printing word40
+    std::cout << word40;
+    //Printing word41
+    std::cout << word41;
+    //Printing word42
+    std::cout << word42;
+    //Printing word43
+    std::cout << word43;
+    //Printing word44
+    std::cout << word44;
+    //Printing word45
+    std::cout << word45;
+    //Printing word46
+    std::cout << word46;
+    //Printing word47
+    std::cout << word47;
+    //Printing word48
+    std::cout << word48;
+    //Printing word49
+    std::cout << word49;
+    //Printing word50
+    std::cout << word50;
+    //Printing word51
+    std::cout << word51;
+    //Printing word52
+    std::cout << word52;
+    //Printing word53
+    std::cout << word53;
+    //Printing word54
+    std::cout << word54;
+    //Printing word55
+    std::cout << word55;
+    //Printing word56
+    std::cout << word56;
+    //Printing word57
+    std::cout << word57;
+    //Printing word58
+    std::cout << word58;
+    //Printing word59
+    std::cout << word59;
+    //Printing word60
+    std::cout << word60;
+    //Printing word61
+    std::cout << word61;
+    //Printing word62
+    std::cout << word62;
+    //Printing word63
+    std::cout << word63;
+    //Printing word64
+    std::cout << word64;
+    //Printing word65
+    std::cout << word65;
+    //Printing word66
+    std::cout << word66;
+    //Printing word67
+    std::cout << word67;
+    //Printing word68
+    std::cout << word68;
+    //Printing word69
+    std::cout << word69;
+    //Printing word70
+    std::cout << word70;
+    //Printing word71
+    std::cout << word71;
+    //Printing word72
+    std::cout << word72;
+    //Printing word73
+    std::cout << word73;
+    //Printing word74
+    std::cout << word74;
+    //Printing word75
+    std::cout << word75;
+    //Printing word76
+    std::cout << word76;
+    //Printing word77
+    std::cout << word77;
+    //Printing word78
+    std::cout << word78;
+    //Printing word79
+    std::cout << word79;
+}
+// This is where the main process happens
+int main()
+{
+    //Unless for loop checking the value of i
+    for (int i = 0; i < 1; i++) {
+        //Seeing if true == true, true != false, false == false and if false != true
+        if (true == true && true != false &&
+            false == false && false != true) {
+            //Calls the display message function
+            Display_message();
+        //Closing curly bracket
+        }
+    //Closing curly bracket
+    }
+//Closing curly bracket
+}
